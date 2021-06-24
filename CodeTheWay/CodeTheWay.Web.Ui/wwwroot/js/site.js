233d1b8c3ae90109175e5b5082ec95bb0ad77d09
Benjamin Kim <22kimb@elmbrookstudenst.org>
2021-06-24 12:44:50 -0500

    HAHAHA GENIUS REBECCA

--- CodeTheWay/CodeTheWay.Web.Ui/wwwroot/js/site.js
+++ CodeTheWay/CodeTheWay.Web.Ui/wwwroot/js/site.js
@@ -24,3 +24,16 @@ function myFunction() {
         }
     }
 }
+var deleteLinks = document.querySelectorAll('.delete');
+
+for (var i = 0; i < deleteLinks.length; i++) {
+    deleteLinks[i].addEventListener('click', function (event) {
+        event.preventDefault();
+
+        var choice = confirm(this.getAttribute('data-confirm'));
+
+        if (choice) {
+            window.location.href = this.getAttribute('href');
+        }
+    });
+}
